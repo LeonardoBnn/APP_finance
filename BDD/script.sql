786c9179c01164f9a344b97d7269f703d2d720bc
LeonardoBnn <boninoleonardo@gmail.com>
2025-03-19 15:10:42 +0100

    Initial commit

---
+++ BDD/script.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE Wealthy;
+
+USE Wealthy;
+
+CREATE TABLE Limite(
+    ID INT auto_increment PRIMARY KEY,
+    Budget float NOT NULL
+);
+
+CREATE TABLE Categorie(
+    ID INT auto_increment PRIMARY KEY,
+    Libelle Varchar(250) NOT NULL,
+    Id_limite int,
+    FOREIGN KEY (Id_limite) REFERENCE LIMITE(ID)
+);
+
+CREATE TABLE Depense(
+    ID INT auto_increment PRIMARY KEY,
+    Montant float NOT NULL,
+    Date_creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    Commentaire Varchar(500),
+    Cat_id int,
+    FOREIGN KEY (Cat_id) REFERENCE CATEGORIE (ID)
+);
+
