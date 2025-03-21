b70c7ee38d6a26e452e95a3a92ad8246ab6c05e8
LeonardoBnn <boninoleonardo@gmail.com>
2025-03-21 13:25:17 +0100

    Update bdd.php

--- BDD/Bdd.php
+++ BDD/Bdd.php
@@ -3,7 +3,7 @@
 try{
     $user='root';
     $pass='';
-    $bdd=new PDO('mysql=host:localhost;dbname=wealthy', $user, $pass);
+    $bdd=new PDO('mysql=host:localhost;dbname=Appdeoaaozuhda', $user, $pass);
 }catch(PDOException $e){
     print 'error :' .$e->getMessage();
     '<br>';
